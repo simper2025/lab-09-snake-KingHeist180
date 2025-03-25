6cf6fa723fefe9845aa5bd3e6dd4cb9c5c2a65b9
Kade Jorgensen1 <kade.jorgensen1@students.snow.edu>
2025-03-25 09:28:34 -0600

    Snake go brrrr

--- SnakeGame/Keyboard.cpp
+++ SnakeGame/Keyboard.cpp
@@ -3,12 +3,16 @@
 
 #define key_UP 72
 #define key_S 115
+
 #define key_LEFT 75
 #define key_W 119
+
 #define key_DOWN 80
 #define key_A 97
+
 #define key_RIGHT 77
 #define key_D 100
+
 #define key_ESCAPE 27
 
 const KeyCommand Keyboard::keyPressed() const
